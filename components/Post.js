5a019c23cf650e65fee90c1706d1a4914982c61a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-07-24 22:55:38 +0900

    Postコンポーネントの中身を記述

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,13 @@
-const Post = () => {
-  return <div></div>;
+const Post = ({ post }) => {
+  return (
+    <div>
+      <span>{post.id}</span>
+      {" : "}
+      <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+        {post.title}
+      </span>
+    </div>
+  );
 };
 
 export default Post;
